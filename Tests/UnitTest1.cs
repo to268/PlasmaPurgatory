10a8c6ceff2d5650400b6def6ba74f9c185bf301
Guillot Tony <tony.guillot@protonmail.com>
2022-01-06 09:37:44 +0100

    Fixed a build error for the Unit Tests Project

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,12 +1,13 @@
 using Xunit;
 
-namespace Tests;
-
-public class UnitTest1
+namespace Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
 
+        }
     }
 }
