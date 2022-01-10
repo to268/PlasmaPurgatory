8897cc0412d92c600064727a0eeba2c8858ff4b1
Thiiboy <thibault.esteves@icloud.com>
2022-01-10 10:08:29 +0100

    Menu update

--- PlasmaPurgatory/Menu.cs
+++ PlasmaPurgatory/Menu.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace PlasmaPurgatory
 {
@@ -13,14 +15,18 @@ namespace PlasmaPurgatory
             public Texture2D texture;
             public Color color;
             public Action callback;
+            public Vector2 origin;
         }
 
+        
+        public const float scale = .3f;
         private Button[] buttons;
         private GraphicsDevice graphicsDevice;
         private SpriteBatch spriteBatch;
         private ContentManager contentManager;
-
-        private const int BUTTONS_COUNT = 1;
+        private MouseState mouseState;
+        private Point mousePosition;
+        private const int BUTTONS_COUNT = 2;
 
         public Menu(GraphicsDevice graphicsDevice, ContentManager contentManager)
         {
@@ -34,8 +40,15 @@ namespace PlasmaPurgatory
         public void Initialize()
         {
             // Start button properties
-            buttons[0].position = new Vector2(0, 0);
+           
+            buttons[0].position = new Vector2(graphicsDevice.Viewport.Width/2, graphicsDevice.Viewport.Height/3);
             buttons[0].color = Color.White;
+
+            buttons[1].position = new Vector2(graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height /2);
+            buttons[1].color = Color.White;
+            
+            mouseState = Mouse.GetState();
+            mousePosition = new Point(mouseState.X, mouseState.Y);
         }
 
         public void LoadContent()
@@ -44,10 +57,20 @@ namespace PlasmaPurgatory
 
             // Start button texture loading
             buttons[0].texture = contentManager.Load<Texture2D>("PlayButton");
+            buttons[0].origin = new Vector2(buttons[0].texture.Width / 2, buttons[0].texture.Height / 2);
+
+            buttons[1].texture = contentManager.Load<Texture2D>("LargeButtons\\LargeButtons\\OptionsButton");
+            buttons[1].origin = new Vector2(buttons[1].texture.Width / 2, buttons[1].texture.Height / 2);
         }
 
         public void Update(GameTime gameTime)
         {
+            if (mouseState.LeftButton == ButtonState.Pressed )
+            {
+                Debug.WriteLine("Boutton clické");
+                buttons[1].texture = contentManager.Load<Texture2D>("LargeButtons\\ColoredLargeButtons\\Optionscol_Button");
+                
+            }
         }
 
         public void Draw(GameTime gameTime)
@@ -56,7 +79,8 @@ namespace PlasmaPurgatory
 
             foreach (Button button in buttons)
             {
-                DrawButton(button);
+                spriteBatch.Draw(button.texture, button.position, null, button.color,
+                                 0, button.origin, scale, SpriteEffects.None, 0f);
             }
 
             spriteBatch.End();
@@ -69,11 +93,6 @@ namespace PlasmaPurgatory
             {
                 buttons[i] = new Button();
             }
-        }
-
-        private void DrawButton(Button button)
-        {
-            spriteBatch.Draw(button.texture, button.position, button.color);
         }
 
     }
