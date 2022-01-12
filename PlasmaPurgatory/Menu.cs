3983080a9e47a4b207a112c2eaf262ed410270b5
Guillot Tony <tony.guillot@protonmail.com>
2022-01-12 22:51:19 +0100

    Fixed the buttons on the menu

--- PlasmaPurgatory/Menu.cs
+++ PlasmaPurgatory/Menu.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
-using MenuBuddy;
 
 namespace PlasmaPurgatory
 {
@@ -13,14 +12,14 @@ namespace PlasmaPurgatory
         struct Button
         {
             public Vector2 position;
-            public Texture2D texture;
+            public Texture2D currentTexture;
+            public Texture2D normalTexture;
+            public Texture2D hoverTexture;
             public Color color;
-            public Action callback;
             public Vector2 origin;
             public Rectangle hitbox;
-
+            public Action callback;
         }
-
         
         public const float scale = .3f;
         private Button[] buttons;
@@ -43,54 +42,42 @@ namespace PlasmaPurgatory
         public void Initialize()
         {
             // Start button properties
-           
-           /* buttons[0].position = new Vector2(graphicsDevice.Viewport.Width/2, graphicsDevice.Viewport.Height/3);
+            buttons[0].position = new Vector2(graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height / 3);
             buttons[0].color = Color.White;
-            
+
 
             buttons[1].position = new Vector2(graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height /2);
-            buttons[1].color = Color.White;*/
-            
-            
-            
+            buttons[1].color = Color.White;
         }
 
         public void LoadContent()
         {
             spriteBatch = new SpriteBatch(graphicsDevice);
 
-            /* Start button texture loading
-            buttons[0].texture = contentManager.Load<Texture2D>("PlayButtonRS");
-            buttons[0].origin = new Vector2(buttons[0].texture.Width / 2, buttons[0].texture.Height / 2);
+            buttons[0].normalTexture = contentManager.Load<Texture2D>("LargeButtons\\LargeButtons\\PlayButton");
+            buttons[0].hoverTexture = contentManager.Load<Texture2D>("LargeButtons\\ColoredLargeButtons\\Playcol_Button");
+            buttons[0].origin = new Vector2(buttons[0].normalTexture.Width / 2, buttons[0].normalTexture.Height / 2);
+            buttons[0].currentTexture = buttons[0].normalTexture;
 
-            buttons[1].texture = contentManager.Load<Texture2D>("LargeButtons\\LargeButtons\\OptionsButton");
-            buttons[1].origin = new Vector2(buttons[1].texture.Width / 2, buttons[1].texture.Height / 2);*/
-        }
-
-        bool EnterButton(Button button)
-        {
-            
-            if (mousePosition.X < button.texture.Bounds.X + button.texture.Width &&
-                    mousePosition.X > button.texture.Bounds.X &&
-                    mousePosition.Y < button.texture.Bounds.Y + button.texture.Height &&
-                    mousePosition.Y > button.texture.Bounds.Y)
-            {
-                return true;
-            }
-            return false;
+            buttons[1].normalTexture = contentManager.Load<Texture2D>("LargeButtons\\LargeButtons\\OptionsButton");
+            buttons[1].hoverTexture = contentManager.Load<Texture2D>("LargeButtons\\ColoredLargeButtons\\Optionscol_Button");
+            buttons[1].origin = new Vector2(buttons[1].normalTexture.Width / 2, buttons[1].normalTexture.Height / 2);
+            buttons[1].currentTexture = buttons[1].normalTexture;
         }
 
         public void Update(GameTime gameTime)
         {
-            
-            
             mouseState = Mouse.GetState();
             mousePosition = new Point(mouseState.X, mouseState.Y);
-            if (mouseState.LeftButton == ButtonState.Pressed && buttons[0].hitbox.Contains(mousePosition))
+
+            for (int i = 0; i < BUTTONS_COUNT; i++)
             {
-                
-                buttons[0].texture = contentManager.Load<Texture2D>("LargeButtons\\ColoredLargeButtons\\Optionscol_Button");
-                Debug.WriteLine(buttons[0].texture.Width + " " + buttons[0].texture.Height);
+                if (mouseState.LeftButton == ButtonState.Pressed && buttons[i].hitbox.Contains(mousePosition))
+                {
+                    buttons[i].currentTexture = buttons[i].hoverTexture;
+                    // TODO: Call the callback function
+                    Debug.WriteLine(buttons[i].normalTexture.Width + " " + buttons[i].normalTexture.Height);
+                }
             }
         }
 
@@ -100,26 +87,20 @@ namespace PlasmaPurgatory
             
             for (int i = 0; i < BUTTONS_COUNT; i++)
             {
-                /*buttons[i].hitbox = new Rectangle((int)buttons[i].position.X, (int)buttons[i].position.Y,
-                                              buttons[i].texture.Width, buttons[i].texture.Height);*/
-                spriteBatch.Draw(buttons[i].texture, buttons[i].position, null, buttons[i].color,
+                buttons[i].hitbox = new Rectangle((int)buttons[i].position.X - 90, (int)buttons[i].position.Y - 80,
+                                                  (int)(buttons[i].normalTexture.Width / 3.2f), (buttons[i].normalTexture.Height / 2) + i * 20);
+
+                spriteBatch.Draw(buttons[i].currentTexture, buttons[i].position, null, buttons[i].color,
                                  0, buttons[i].origin, scale, SpriteEffects.None, 0f);
-                
             }
 
             spriteBatch.End();
         }
 
-
         private void PopulateButtonArray()
         {
             for (int i = 0; i < buttons.Length; i++)
-            {
                 buttons[i] = new Button();
-            }
         }
-
-        
-
     }
 }
